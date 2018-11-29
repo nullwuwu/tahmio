2443cf04e6f899348a2c64b817cdfddb7f519537
caominjie <23077773@qq.com>
2018-11-29 17:19:22 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,27 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel';
 import pkg from './package.json'
 
 const input = 'lib/index.js'
 
-export default {
-  input,
-  output: [{
-    file: pkg.module,
-    format: 'es'
-  }, {
-    file: pkg.browser,
-    format: 'umd',
-    name: 'tham'
+export default [
+  {
+    input,
+    output: {
+      name: 'tham',
+      file: pkg.browser,
+      format: 'umd'
     },
-    { file: pkg.main, format: 'cjs' },],
-  plugins: [
-    resolve(),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: true
-    })
-  ]
-};
-
-
-
-// export default [
-//   {
-//     input,
-//     output: {
-//       name: 'Tank',
-//       file: pkg.browser,
-//       format: 'umd'
-//     },
-//     plugins: [
-//       resolve(),
-//       commonjs()
-//     ]
-//   },
-//   {
-//     input,
-//     output: [
-//       { file: pkg.main, format: 'cjs' },
-//       { file: pkg.module, format: 'es' }
-//     ]
-//   }
-// ];
+    plugins: [
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input,
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ]
+  }
+];
